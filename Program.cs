6ad3b19a7219b966d31209866371eb5830927198
Vladyslav <vladyslavstratiichuk@gmail.com>
2026-05-22 08:22:35 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,220 +1,183 @@
 ﻿using System;
 
-// ===================== TASK 1 =====================
+// TASK 1
 
-Console.WriteLine("===== TASK 1 =====");
+Console.WriteLine("TASK 1");
+
+Func<string, string> rainbowColor = delegate (string color)
+{
+    switch (color.ToLower())
+    {
+        case "red":
+            return "RGB(255, 0, 0)";
+
+        case "orange":
+            return "RGB(255, 165, 0)";
+
+        case "yellow":
+            return "RGB(255, 255, 0)";
+
+        case "green":
+            return "RGB(0, 255, 0)";
+
+        case "blue":
+            return "RGB(0, 0, 255)";
+
+        case "indigo":
+            return "RGB(75, 0, 130)";
+
+        case "violet":
+            return "RGB(238, 130, 238)";
+
+        default:
+            return "Color not found";
+    }
+};
+
+Console.Write("Enter rainbow color: ");
+string userColor = Console.ReadLine();
+
+Console.WriteLine(rainbowColor(userColor));
+
+Console.WriteLine();
+
+
+// TASK 2
+
+Console.WriteLine("TASK 2");
+
+Backpack backpack = new Backpack();
+
+backpack.ItemAdded += delegate (string item)
+{
+    Console.WriteLine(item + " added to backpack");
+};
+
+backpack.ItemRemoved += delegate (string item)
+{
+    Console.WriteLine(item + " removed from backpack");
+};
+
+backpack.Changed += delegate ()
+{
+    Console.WriteLine("Backpack changed");
+};
 
 try
 {
-    Console.Write("Enter square size: ");
-    int size = int.Parse(Console.ReadLine());
+    backpack.Color = "Black";
+    backpack.Brand = "Nike";
+    backpack.Fabric = "Polyester";
+    backpack.Weight = 1.5;
+    backpack.Volume = 5;
 
-    Console.Write("Enter symbol: ");
-    char symbol = char.Parse(Console.ReadLine());
+    backpack.AddItem("Book");
+    backpack.AddItem("Laptop");
+    backpack.RemoveItem("Book");
 
-    DrawSquare(size, symbol);
+    backpack.Show();
 }
-catch
+catch (Exception ex)
 {
-    Console.WriteLine("Input error!");
+    Console.WriteLine(ex.Message);
 }
 
 Console.WriteLine();
 
 
-// ===================== TASK 4 =====================
+// TASK 3
 
-Console.WriteLine("===== TASK 4 =====");
+Console.WriteLine("TASK 3");
 
-Website site = new Website();
-site.Input();
-site.Output();
+int[] numbers = { 7, 14, 21, 5, 9, 28, 35 };
 
-Console.WriteLine();
-
-
-// ===================== TASK 5 =====================
-
-Console.WriteLine("===== TASK 5 =====");
-
-Magazine magazine = new Magazine();
-magazine.Input();
-magazine.Output();
-
-
-// ===================== METHODS =====================
-
-void DrawSquare(int size, char symbol)
+Func<int, int> countNumbers = (x) =>
 {
-    for (int i = 0; i < size; i++)
+    int count = 0;
+
+    foreach (int n in numbers)
     {
-        for (int j = 0; j < size; j++)
+        if (n % x == 0)
+            count++;
+    }
+
+    return count;
+};
+
+Console.WriteLine("Numbers divisible by 7: " + countNumbers(7));
+Console.WriteLine("Numbers divisible by 5: " + countNumbers(5));
+Console.WriteLine("Numbers divisible by 3: " + countNumbers(3));
+
+
+
+// CLASS
+
+class Backpack
+{
+    public string Color { get; set; }
+    public string Brand { get; set; }
+    public string Fabric { get; set; }
+    public double Weight { get; set; }
+    public int Volume { get; set; }
+
+    private string[] items = new string[100];
+    private int itemCount = 0;
+
+    public delegate void BackpackHandler(string item);
+
+    public event BackpackHandler ItemAdded;
+    public event BackpackHandler ItemRemoved;
+
+    public delegate void ChangeHandler();
+
+    public event ChangeHandler Changed;
+
+    public void AddItem(string item)
+    {
+        if (itemCount >= Volume)
+            throw new Exception("Backpack is full");
+
+        items[itemCount] = item;
+        itemCount++;
+
+        ItemAdded?.Invoke(item);
+        Changed?.Invoke();
+    }
+
+    public void RemoveItem(string item)
+    {
+        for (int i = 0; i < itemCount; i++)
         {
-            Console.Write(symbol);
+            if (items[i] == item)
+            {
+                for (int j = i; j < itemCount - 1; j++)
+                {
+                    items[j] = items[j + 1];
+                }
+
+                itemCount--;
+
+                ItemRemoved?.Invoke(item);
+                Changed?.Invoke();
+
+                break;
+            }
         }
-        Console.WriteLine();
-    }
-}
-
-
-// ===================== CLASS WEBSITE =====================
-
-class Website
-{
-    private string name;
-    private string path;
-    private string description;
-    private string ip;
-
-    public void Input()
-    {
-        Console.Write("Enter website name: ");
-        name = Console.ReadLine();
-
-        Console.Write("Enter website path: ");
-        path = Console.ReadLine();
-
-        Console.Write("Enter website description: ");
-        description = Console.ReadLine();
-
-        Console.Write("Enter website IP: ");
-        ip = Console.ReadLine();
     }
 
-    public void Output()
+    public void Show()
     {
-        Console.WriteLine("Website name: " + name);
-        Console.WriteLine("Website path: " + path);
-        Console.WriteLine("Website description: " + description);
-        Console.WriteLine("Website IP: " + ip);
-    }
+        Console.WriteLine("Color: " + Color);
+        Console.WriteLine("Brand: " + Brand);
+        Console.WriteLine("Fabric: " + Fabric);
+        Console.WriteLine("Weight: " + Weight);
+        Console.WriteLine("Volume: " + Volume);
 
-    public void SetName(string name)
-    {
-        this.name = name;
-    }
+        Console.WriteLine("Items:");
 
-    public string GetName()
-    {
-        return name;
-    }
-
-    public void SetPath(string path)
-    {
-        this.path = path;
-    }
-
-    public string GetPath()
-    {
-        return path;
-    }
-
-    public void SetDescription(string description)
-    {
-        this.description = description;
-    }
-
-    public string GetDescription()
-    {
-        return description;
-    }
-
-    public void SetIp(string ip)
-    {
-        this.ip = ip;
-    }
-
-    public string GetIp()
-    {
-        return ip;
-    }
-}
-
-
-// ===================== CLASS MAGAZINE =====================
-
-class Magazine
-{
-    private string name;
-    private int year;
-    private string description;
-    private string phone;
-    private string email;
-
-    public void Input()
-    {
-        Console.Write("Enter magazine name: ");
-        name = Console.ReadLine();
-
-        Console.Write("Enter foundation year: ");
-        year = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter magazine description: ");
-        description = Console.ReadLine();
-
-        Console.Write("Enter contact phone: ");
-        phone = Console.ReadLine();
-
-        Console.Write("Enter contact email: ");
-        email = Console.ReadLine();
-    }
-
-    public void Output()
-    {
-        Console.WriteLine("Magazine name: " + name);
-        Console.WriteLine("Foundation year: " + year);
-        Console.WriteLine("Description: " + description);
-        Console.WriteLine("Phone: " + phone);
-        Console.WriteLine("Email: " + email);
-    }
-
-    public void SetName(string name)
-    {
-        this.name = name;
-    }
-
-    public string GetName()
-    {
-        return name;
-    }
-
-    public void SetYear(int year)
-    {
-        this.year = year;
-    }
-
-    public int GetYear()
-    {
-        return year;
-    }
-
-    public void SetDescription(string description)
-    {
-        this.description = description;
-    }
-
-    public string GetDescription()
-    {
-        return description;
-    }
-
-    public void SetPhone(string phone)
-    {
-        this.phone = phone;
-    }
-
-    public string GetPhone()
-    {
-        return phone;
-    }
-
-    public void SetEmail(string email)
-    {
-        this.email = email;
-    }
-
-    public string GetEmail()
-    {
-        return email;
+        for (int i = 0; i < itemCount; i++)
+        {
+            Console.WriteLine("- " + items[i]);
+        }
     }
 }
